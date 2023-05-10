b10cac94848cdac806a637c7d98c96213c4d079b
Jonas Schen <jonas@crmpiperun.com>
2023-05-10 16:44:22 -0300

    Text implemention

--- tests/Tests/Unit/Console/Commands/LangMonitorScanCommandTest.php
+++ tests/Tests/Unit/Console/Commands/LangMonitorScanCommandTest.php
@@ -9,10 +9,7 @@ final class LangMonitorScanCommandTest extends TestCase
 {
     public function test_it_scan()
     {
-        //$command = $this->artisan('lang_monitor:scan');
-        //$command->assertExitCode(0);
-        $a = '123';
-        $b = '123';
-        $this->assertTrue($a === $b);
+        $command = $this->artisan('lang_monitor:scan');
+        $command->assertExitCode(0);
     }
 }
